313ed3fa6ef845a069c2b81fc0e94cb88720da13
AjoobKansakar <ajoob.sagar@gmail.com>
2026-02-19 23:37:47 +0545

    Supabase connection added for user authentication

--- kasrrat/lib/main.dart
+++ kasrrat/lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'routes/app_routes.dart';
+// Supabase Import
+import 'package:supabase_flutter/supabase_flutter.dart';
 // import 'screens/login.dart';
 // import 'screens/signup.dart';
 
-void main() {
+Future<void> main() async {
+  // Flutter cloud connection
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Supabase
+  await Supabase.initialize(
+    url: 'https://qdoprrnffikvwlkdcccx.supabase.co',
+    anonKey: 'sb_publishable_Gn1H7BM8cABc85ajouVeOw_-F4gINdS',
+  );
+
   runApp(const KasRratApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class KasRratApp extends StatelessWidget {
   const KasRratApp({super.key});
@@ -16,13 +29,12 @@ class KasRratApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'KasRrat',
       theme: ThemeData.dark(),
-  
-      // show the login screen first
-      initialRoute: AppRoutes.login, 
 
-      // maping for navigation 
-      routes: AppRoutes.getRoutes(),  
-      
+      // show the login screen first
+      initialRoute: AppRoutes.login,
+
+      // maping for navigation
+      routes: AppRoutes.getRoutes(),
     );
   }
 }

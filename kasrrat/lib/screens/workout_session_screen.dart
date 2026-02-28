598e9eafc7981ce619e92cb35ad11327959303f7
AjoobKansakar <ajoob.sagar@gmail.com>
2026-02-28 22:48:25 +0545

    working camera feature added to the application

---
+++ kasrrat/lib/screens/workout_session_screen.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+// Camera Package import
+import 'package:camera/camera.dart'; 
+import '../core/kasrrat_colors.dart';
+
+class WorkoutSessionScreen extends StatefulWidget {
+  final String exerciseName;
+
+  const WorkoutSessionScreen({super.key, required this.exerciseName});
+
+  @override
+  State<WorkoutSessionScreen> createState() => _WorkoutSessionScreenState();
+}
+
+class _WorkoutSessionScreenState extends State<WorkoutSessionScreen> {
+  // Camera variables
+  CameraController? _controller; // camera hardware
+  bool _isInitialized = false;  // to track if camera is ready
+
+  @override
+  void initState() {
+    super.initState();
+    _setupCamera(); // sets the camera up when the screen starts
+  }
+
+  // camera access logic
+  Future<void> _setupCamera() async {
+      try {
+        // available cameras
+        final cameras = await availableCameras();
+        
+        if (cameras.isEmpty) {
+          debugPrint("No cameras found on this device");
+          return;
+        }
+
+        // Camera User
+        CameraDescription selectedCamera = cameras.first;
+        for (var cam in cameras) {
+          if (cam.lensDirection == CameraLensDirection.front) {
+            selectedCamera = cam;
+            break;
+          }
+        }
+
+        // Initializing controller
+        _controller = CameraController(
+          selectedCamera,
+          ResolutionPreset.medium,
+          enableAudio: false,
+        );
+
+        await _controller!.initialize();
+
+        // update state after initialization
+        if (mounted) {
+          setState(() {
+            _isInitialized = true;
+          });
+        }
+      } catch (e) {
+        // Error msg
+        debugPrint("KASRRAT CAMERA ERROR: $e");
+      }
+    }
+
+  @override
+  void dispose() {
+    //Turn off camera when screen existed
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  // UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          // Live Camera widget
+          _isInitialized
+              ? Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller!.value.aspectRatio,
+                    child: CameraPreview(_controller!), // Shows actual video
+                  ),
+                )
+              : const Center(
+                  child: CircularProgressIndicator(color: AppColors.primary),
+                ),
+
+          // overlay
+          SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      Text(
+                        widget.exerciseName.toUpperCase(),
+                        style: const TextStyle(
+                          color: Colors.white, 
+                          fontSize: 20, 
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2
+                        ),
+                      ),
+                      const SizedBox(width: 40),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                
+                // Rep Counter
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: AppColors.primary.withValues(alpha: 0.8),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text(
+                    "REPS: 0",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 50),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
